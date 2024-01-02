d6b711b4d0cd730ab312b7577580beb2382e0dda
Arunkumar Selvaraj <arun@ioak.io>
2024-01-02 14:27:03 +0530

    base version

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import * as AuthenticationService from './services/AuthenticationService';
-import * as AuthliteTypes from './components/types';
-import * as AuthliteComponents from './ComponentsIndex';
-
-export {
-    AuthenticationService as AuthliteAuthenticationService,
-    AuthliteTypes,
-    AuthliteComponents,
-};
